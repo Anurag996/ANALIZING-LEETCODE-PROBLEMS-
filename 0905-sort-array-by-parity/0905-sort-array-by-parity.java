256683ba8eba24d8a6e61fa2059f23d850bafe9d
ANURAG PRAJAPATI <anurag.prajapati_cs25@gla.ac.in>
2026-08-13 16:45:25 +0530

    Time: 0 ms (100%), Space: 46.4 MB (97.05%) - LeetHub

---
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        
+        int st=0;
+        int end= nums.length-1;
+        int[] arr= new int[nums.length];
+     for(int i =0;i< nums.length;i++){
+
+        
+       if(nums[i]%2==0){
+            arr[st]=nums[i];
+
+            st++;
+        }
+      else{
+        arr[end]=nums[i];
+        end--;
+     }
+    }
+    
+    
+
+return arr;
+    }
+}
