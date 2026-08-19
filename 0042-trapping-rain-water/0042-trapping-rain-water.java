33fc2c65de7dfc4c4defbe6dc331740fe623eb0b
ANURAG PRAJAPATI <anurag.prajapati_cs25@gla.ac.in>
2026-08-19 16:09:57 +0530

    Time: 1 ms (58.52%), Space: 48.9 MB (9.18%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,27 +1,52 @@
 class Solution {
     public int trap(int[] height) {
-        int left = 0, right = height.length - 1;
-        int leftMax = 0, rightMax = 0;
-        int water = 0;
+        // int left = 0, right = height.length - 1;
+        // int leftMax = 0, rightMax = 0;
+        // int water = 0;
 
-        while (left < right) {
-            if (height[left] < height[right]) {
-                if (height[left] >= leftMax) {
-                    leftMax = height[left];
-                } else {
-                    water += leftMax - height[left];
-                }
-                left++;
-            } else {
-                if (height[right] >= rightMax) {
-                    rightMax = height[right];
-                } else {
-                    water += rightMax - height[right];
-                }
-                right--;
-            }
-        }
+        // while (left < right) {
+        //     if (height[left] < height[right]) {
+        //         if (height[left] >= leftMax) {
+        //             leftMax = height[left];
+        //         } else {
+        //             water += leftMax - height[left];
+        //         }
+        //         left++;
+        //     } else {
+        //         if (height[right] >= rightMax) {
+        //             rightMax = height[right];
+        //         } else {
+        //             water += rightMax - height[right];
+        //         }
+        //         right--;
+        //     }
+        // }
 
-        return water;
+        // return water;
+
+
+// prefix
+int sum=0;
+    int n= height.length;
+        int[] left = new int[n];
+        left[0]=  height[0];
+   for( int i=1;i< left.length;i++){
+            left[i]= Math.max(left[i-1],height[i]);
+   }
+  //suffix
+
+   int[] right = new int[n];
+        right[n-1]=  height[n-1];
+   for( int i=n-2;i>=0;i--){
+            right[i]= Math.max(right[i+1],height[i]);
+   }
+      
+
+
+      for(int i=0;i<n;i++){
+        sum += (Math.min(left[i],right[i]))- height[i];
+      }
+
+      return sum ;
     }
 }
