113fccdd895a5dcabde0807d2933578621c40140
ANURAG PRAJAPATI <anurag.prajapati_cs25@gla.ac.in>
2026-09-12 23:11:50 +0530

    Time: 6 ms (20.71%), Space: 46.5 MB (74.83%) - LeetHub

---
+++ 4048-count-values-with-equally-spaced-occurrences-i/4048-count-values-with-equally-spaced-occurrences-i.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int countSpecialIntegers(int[] nums) {
+
+        int ans = 0;
+
+        for (int i = 0; i < 101; i++) {
+
+            int count = 0;
+            int first = -1, second = -1, third = -1;
+
+            for (int j = 0; j < nums.length; j++) {
+
+                if (nums[j] == i) {
+                    count++;
+
+                    if (count == 1)
+                        first = j;
+                    else if (count == 2)
+                        second = j;
+                    else if (count == 3)
+                        third = j;
+                }
+            }
+
+            if (count == 3 && second - first == third - second)
+                ans++;
+        }
+
+        return ans;
+    }
+}
