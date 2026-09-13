70d3ae707a8163d6c1f562b55b43d84efd76c840
ANURAG PRAJAPATI <anurag.prajapati_cs25@gla.ac.in>
2026-09-13 21:54:42 +0530

    Time: 1 ms (100%), Space: 47.2 MB (95.37%) - LeetHub

---
+++ 4052-cyclically-shift-rows-and-columns/4052-cyclically-shift-rows-and-columns.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int[][] cyclicShift(int n, int[][] grid, int[] rowShift, int[] colShift) {
+        
+
+         // row shift 
+  //int n = grid.length;
+        for(int i=0;i<n;i++){
+            int k= rowShift[i]%n;
+            int[] temp = new int[n];
+        
+        for (int j=0 ;j<n;j++){
+           
+                temp[j] = grid[i][(j + k) % n];
+            
+        }
+        for(int j=0;j<n;j++){
+            grid[i][j]=temp[j];
+        }
+        }
+
+
+
+        // couln shift 
+          for(int j=0;j<n;j++){
+            int k= colShift[j]%n;
+            int[] temp = new int[n];
+        
+        for (int i=0 ;i<n;i++){
+           
+            temp[i] = grid[(i + k) % n][j];
+            
+        }
+        for(int i=0;i<n;i++){
+            grid[i][j]=temp[i];
+        }
+      }
+ 
+return grid ;
+
+
+    }
+}
