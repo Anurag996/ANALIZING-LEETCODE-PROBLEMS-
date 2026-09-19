34fc0e8d59c68a29a4afc2291b6dd6024a8df17e
ANURAG PRAJAPATI <anurag.prajapati_cs25@gla.ac.in>
2026-09-19 13:35:36 +0530

    Time: 1 ms (99.93%), Space: 77.1 MB (82.76%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,25 +1,21 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-
-        // currentSum = abhi tak ka running sum
-        // maxSum = ab tak mila sabse bada sum
-        int currentSum = nums[0];
-        int maxSum = nums[0];
-
-        // second element se loop start karte hain
-        for (int i = 1; i < nums.length; i++) {
-
         
-            //  Naya subarray yahin se start karo (nums[i])
-            //  Ya previous sum me current element jod do
+
+        int cs= 0;
+        int ms= Integer.MIN_VALUE;
+
+        for(int i=0;i<nums.length;i++){
             
-            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            
+            cs=cs+nums[i];
+            ms= Math.max(ms,cs);
 
-            // Agar currentSum bada hai maxSum se,
-            // to maxSum update kar do
-            maxSum = Math.max(maxSum, currentSum);
+            if(cs<0){
+                cs=0;
+            }
         }
-
-        return maxSum;
+    
+    return ms;
     }
 }
