28eeebad4c390370a31a3c863be3349e1903d328
ANURAG PRAJAPATI <anurag.prajapati_cs25@gla.ac.in>
2026-09-20 19:04:31 +0530

    Time: 0 ms (100%), Space: 53 MB (22.16%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,13 +1,56 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
         
-  int xor=0;
+//   int xor=0;
 
 
-        for(int i=0;i<nums.length;i++){
-     xor=xor^nums[i];
-        }
+//         for(int i=0;i<nums.length;i++){
+//      xor=xor^nums[i];
+//         }
    
-   return xor;
+//    return xor;
+
+  int n = nums.length;
+     int si=0;
+     int ei=nums.length-1;
+     int ans=0;
+
+
+     if(n==1){
+        return nums[0];
+     }
+
+     while(si<=ei){
+        int mid= si+ (ei-si)/2;
+     
+
+        if(mid==0 && nums[0]!=nums[1]){
+            return nums[0];
+        }
+        if(mid==n-1 &&  nums[n-1] != nums[n-2]){
+            return nums[n-1];
+        }
+        if(nums[mid-1]!= nums[mid] && nums[mid] != nums[mid+1]){
+             ans= nums[mid];
+            return nums[mid];
+        }
+
+        if(mid%2==0){
+            if(nums[mid-1]==nums[mid]){
+                ei=mid-1;
+            }else{
+                si=mid+1;
+            }
+        }else{
+            if(nums[mid-1]==nums[mid]){
+                si=mid+1;
+            }else{
+                ei=mid-1;
+            }
+        }
+     }
+    
+ return ans;
+
     }
 }
