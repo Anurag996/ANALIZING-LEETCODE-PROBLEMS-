f81bcab4ad4b0d9bf14300292679076eabaaf7a1
ANURAG PRAJAPATI <anurag.prajapati_cs25@gla.ac.in>
2026-09-21 23:22:07 +0530

    Time: 3 ms (85.22%), Space: 48.2 MB (64.52%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -2,10 +2,13 @@ class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         
 
-    int row=0;
-    int col= matrix[0].length-1;
 
-    while(row < matrix.length && col>=0){
+///   STAIRCASE ALGORITHM USE HUAH HAI 
+
+    int row=0;
+    int col= matrix[0].length-1;    /// YAHAN SE START KRO 
+
+    while(row < matrix.length && col>=0){  /// YE LINE IMP HAI 
         if(matrix[row][col]==target){
             return true;
         }else if(matrix[row][col]>target){
